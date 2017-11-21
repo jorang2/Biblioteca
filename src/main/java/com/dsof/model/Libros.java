f0592cd7f356c8b26a8927f41b9622d9bc61bf93
Jorge Enrique Angarita Barona <jeangarita@enertotalesp.local>
2017-11-20 19:09:31 -0500

    PRUEBA

--- src/main/java/com/dsof/model/Libros.java
+++ src/main/java/com/dsof/model/Libros.java
@@ -14,7 +14,7 @@ import javax.validation.constraints.*;
 /**
 * @author Zathura Code Generator http://zathuracode.org
 * www.zathuracode.org
-*
+*   PRUEBA DE GIT
 */
 @Entity
 @Table(name = "libros", schema = "${schema}")
